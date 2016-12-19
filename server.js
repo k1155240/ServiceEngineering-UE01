fd6f6ad14ba981120d54cba46165ef70994ed430
k1155240 <k1155240@students.jku.at>
2016-12-19 18:30:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-}).listen(process.env.PORT || 8080);
+var express = require("express");
+var path = require("path");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({ secret: 'thatsfunny' }));
+
+app.get("/",function(req,res,next){
+  //res.sendFile(path.resolve(__dirname + '/../app//index.html'));
+  res.send("Hello world " + __dirname);
+}); 
+app.get("/index.html",function(req,res,next){
+    console.log(req.user);
+  //res.sendFile(path.resolve(__dirname + '/../app//index.html'));
+  res.send("Hello world " + __dirname);
+}); 
+
+app.listen(process.env.PORT,function(){
+    console.log("Started listening on port", process.env.PORT);
+});
